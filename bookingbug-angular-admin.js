ebbefe30d5fddbaadec31efba3c5d7e002bf04cb
edclements <ed.clements@gmail.com>
2016-10-18 17:43:09 +0100

    Version v2.0.39

--- bookingbug-angular-admin.js
+++ bookingbug-angular-admin.js
@@ -2806,20 +2806,6 @@
             SEND_EMAIL: 'Send cancellation confirmation to {{email}}?',
             TITLE: 'Cancel Booking'
           }
-        },
-        LOGIN: {
-          EMAIL_LABEL: 'Email',
-          EMAIL_PLACEHOLDER: 'Email',
-          PASSWORD_LABEL: 'Password',
-          PASSWORD_PLACEHOLDER: 'Password',
-          LOGIN_BTN: 'Login'
-        },
-        PICK_COMPANY: {
-          STEP_SUMMARY: 'Pick Company'
-        },
-        BOOKNG_TABLE: {
-          NEW_BOOKING_BTN: 'New booking',
-          EDIT_BTN: 'Edit'
         }
       }
     };
